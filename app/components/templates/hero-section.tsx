48dda1cc80fc58ec44bc997a23ca85bf81e50bb8
SHEKHAR GUPTA <137384631+shekhar-sg@users.noreply.github.com>
2025-02-27 21:24:37 +0530

    update styles and structure: add button, link, and typography styles; enhance hero section with feature banner

--- app/components/templates/hero-section.tsx
+++ app/components/templates/hero-section.tsx
@@ -1,12 +1,63 @@
 import SectionWrapper from "~/components/atoms/section-wrapper";
-import { Link } from "react-router";
+import { IoChatbubblesOutline } from "react-icons/io5";
+import { BsShieldCheck } from "react-icons/bs";
+import { FaUserDoctor } from "react-icons/fa6";
+import { MdEditDocument } from "react-icons/md";
 
 const HeroSection = () => {
   return (
     <SectionWrapper>
-      <h1>Hero section</h1>
+      <div></div>
+      <div></div>
+      <Banner />
     </SectionWrapper>
   );
 };
 
 export default HeroSection;
+
+const features = [
+  {
+    icon: IoChatbubblesOutline,
+    about: "Convenient online & In-clinic consultations",
+  },
+  {
+    icon: BsShieldCheck,
+    about: "Safe and effective treatment",
+  },
+  {
+    icon: FaUserDoctor,
+    about: "Experienced Ayurvedic Practitioners",
+  },
+  {
+    icon: MdEditDocument,
+    about: "Personalized Treatment Plans & Guidance",
+  },
+];
+
+const Banner = () => {
+  return (
+    <div className={"flex items-center shadow"}>
+      {features.map((feature, index) => {
+        const Icon = feature.icon;
+        return (
+          <div
+            key={index}
+            className={
+              "text-primary-main flex items-center gap-4 border-r-amber-300"
+            }
+          >
+            <div
+              className={
+                "flex aspect-square p-3 rounded-full border"
+              }
+            >
+              <Icon className={"size-10"} />
+            </div>
+            <p>{feature.about}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
