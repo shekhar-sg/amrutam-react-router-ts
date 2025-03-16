fece3b4953f0b1fc5ffff3a48ea994901f231685
SHEKHAR GUPTA <137384631+shekhar-sg@users.noreply.github.com>
2025-03-16 08:06:57 +0530

    feat: update DiscoverAyurveda component; refactor layout structure and improve scroll handling

--- app/components/templates/discover-ayurveda/index.tsx
+++ app/components/templates/discover-ayurveda/index.tsx
@@ -9,7 +9,7 @@ import { useMediaQuery } from "react-responsive";
 const { heading, chakra, meditation, description, cards } = DiscoverSectionData;
 
 const DiscoverAyurveda = () => {
-  const ref = useRef<HTMLElement>(null);
+  const ref = useRef<HTMLDivElement>(null);
   const isMobile = useMediaQuery({ maxWidth: 1024 });
   const { scrollYProgress } = useScroll({
     target: ref,
@@ -19,7 +19,7 @@ const DiscoverAyurveda = () => {
 
   const chakraOpacity = useTransform(
     scrollYProgress,
-    [0.1, 0.35, 0.6],
+    [0.2, 0.35, 0.6],
     [0, 0.4, 1],
   );
   const chakraScale = useTransform(scrollYProgress, [0.1, 0.6], [20, 1]);
@@ -49,110 +49,112 @@ const DiscoverAyurveda = () => {
   );
 
   return (
-    <SectionWrapper
-      ref={ref}
-      WrapperProps={{
-        className: `pt-30 calc-height-200`,
-      }}
-      className={`calc-height-100 sticky top-[var(--header-height)] mt-54 flex min-h-fit flex-col items-center gap-5 md:mt-10`}
-    >
-      <div
-        className={
-          "flex flex-col items-center justify-center gap-5 text-center"
-        }
+    <div ref={ref} className={"h-[400vh]"}>
+      <SectionWrapper
+        WrapperProps={{
+          className:
+            "h-screen sticky top-0 overflow-hidden pt-[var(--header-height)]",
+        }}
+        className={"flex h-full flex-col items-center gap-5"}
       >
-        <h2
-          className={"text-primary-main border-b-8 px-6 leading-15 font-bold"}
+        <div
+          className={
+            "flex flex-col items-center justify-center gap-5 text-center"
+          }
         >
-          {heading}
-        </h2>
-        <p className={"max-w-2xl"}>{description}</p>
-      </div>
-      <div
-        className={
-          "flex h-full flex-col items-center justify-center gap-y-10 lg:flex-row xl:gap-5"
-        }
-      >
-        <div className={"hidden flex-1 flex-col items-end gap-15 lg:flex"}>
-          {cards.slice(0, 3).map((card, index) => {
-            return (
-              <YogaCards
-                key={card.title}
-                image={card.icon}
-                title={card.title}
-                description={card.description}
-                className={clsx(
-                  "text-right lg:flex-row-reverse",
-                  index % 2 && "mr-8",
-                )}
-                style={{
-                  x: yogaCardLeftXPosition,
-                  scale: yogaCardScale,
-                  opacity: yogaCardOpacity,
-                }}
-              />
-            );
-          })}
+          <h2
+            className={"text-primary-main border-b-8 px-6 leading-15 font-bold"}
+          >
+            {heading}
+          </h2>
+          <p className={"max-w-2xl"}>{description}</p>
         </div>
         <div
           className={
-            "relative max-h-[50vw] w-[50vw] flex-1 lg:h-auto lg:w-auto"
+            "flex h-full flex-col items-center justify-center gap-y-10 lg:flex-row xl:gap-5"
           }
         >
-          <motion.img
-            src={chakra}
-            alt={"yoga"}
-            whileInView={{
-              rotate: 360,
-              transition: {
-                duration: 100,
-                repeat: Infinity,
-                ease: "linear",
-              },
-            }}
-            style={{
-              opacity: chakraOpacity,
-              scale: chakraScale,
-              y: chakraYPosition,
-            }}
-          />
-          <img
-            src={meditation}
-            alt={"yoga"}
-            className={"absolute -bottom-1/6 -left-2"}
-          />
+          <div className={"hidden flex-1 flex-col items-end gap-15 lg:flex"}>
+            {cards.slice(0, 3).map((card, index) => {
+              return (
+                <YogaCards
+                  key={card.title}
+                  image={card.icon}
+                  title={card.title}
+                  description={card.description}
+                  className={clsx(
+                    "text-right lg:flex-row-reverse",
+                    index % 2 && "mr-8",
+                  )}
+                  style={{
+                    x: yogaCardLeftXPosition,
+                    scale: yogaCardScale,
+                    opacity: yogaCardOpacity,
+                  }}
+                />
+              );
+            })}
+          </div>
+          <div
+            className={
+              "relative max-h-[50vw] w-[50vw] flex-1 lg:h-auto lg:w-auto"
+            }
+          >
+            <motion.img
+              src={chakra}
+              alt={"yoga"}
+              whileInView={{
+                rotate: 360,
+                transition: {
+                  duration: 100,
+                  repeat: Infinity,
+                  ease: "linear",
+                },
+              }}
+              style={{
+                opacity: chakraOpacity,
+                scale: chakraScale,
+                y: chakraYPosition,
+              }}
+            />
+            <img
+              src={meditation}
+              alt={"yoga"}
+              className={"absolute -bottom-1/6 -left-2"}
+            />
+          </div>
+          <div
+            className={
+              "flex flex-row flex-wrap justify-center gap-5 lg:flex-1 lg:flex-col lg:gap-15"
+            }
+          >
+            {cards.map((card, index) => {
+              return (
+                <YogaCards
+                  key={card.title}
+                  image={card.icon}
+                  title={card.title}
+                  description={card.description}
+                  className={clsx(
+                    "!w-[calc((100%-20px)/2)] flex-col text-center lg:!w-full lg:flex-row lg:text-start",
+                    {
+                      "lg:hidden": index < 3,
+                      "lg:ml-8": !(index % 2),
+                    },
+                  )}
+                  style={{
+                    y: isMobile ? yogaCardMobileYPosition : 0,
+                    x: isMobile ? 0 : yogaCardRightXPosition,
+                    scale: yogaCardScale,
+                    opacity: yogaCardOpacity,
+                  }}
+                />
+              );
+            })}
+          </div>
         </div>
-        <div
-          className={
-            "flex flex-row flex-wrap justify-center gap-5 lg:flex-1 lg:flex-col lg:gap-15"
-          }
-        >
-          {cards.map((card, index) => {
-            return (
-              <YogaCards
-                key={card.title}
-                image={card.icon}
-                title={card.title}
-                description={card.description}
-                className={clsx(
-                  "!w-[calc((100%-20px)/2)] flex-col text-center lg:!w-full lg:flex-row lg:text-start",
-                  {
-                    "lg:hidden": index < 3,
-                    "lg:ml-8": !(index % 2),
-                  },
-                )}
-                style={{
-                  y: isMobile ? yogaCardMobileYPosition : 0,
-                  x: isMobile ? 0 : yogaCardRightXPosition,
-                  scale: yogaCardScale,
-                  opacity: yogaCardOpacity,
-                }}
-              />
-            );
-          })}
-        </div>
-      </div>
-    </SectionWrapper>
+      </SectionWrapper>
+    </div>
   );
 };
 
