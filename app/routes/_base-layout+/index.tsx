20ee16bef667978d2d876a3f6fa2cd5601f45ce6
SHEKHAR GUPTA <137384631+shekhar-sg@users.noreply.github.com>
2025-03-17 01:19:25 +0530

    feat: remove DownloadApp component import from index.tsx

--- app/routes/_base-layout+/index.tsx
+++ app/routes/_base-layout+/index.tsx
@@ -2,7 +2,6 @@ import DiscoverAyurveda from "~/components/templates/discover-ayurveda";
 import Stats from "~/components/templates/stats";
 import Experts from "~/components/templates/experts";
 import Video from "~/components/templates/video";
-import DownloadApp from "~/components/templates/download-app";
 import Testimonials from "~/components/templates/testimonials";
 import HeroSection from "~/components/templates/hero-section";
 import ConsultationProcess from "~/components/templates/consultation-process";
