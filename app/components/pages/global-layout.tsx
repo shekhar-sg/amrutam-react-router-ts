689974c9debf3a8c229f509f5b2e8e608e85b028
suraj <37866971+srgsid@users.noreply.github.com>
2025-03-17 01:59:46 +0530

    feat: integrate MantineProvider and create custom theme

--- app/components/pages/global-layout.tsx
+++ app/components/pages/global-layout.tsx
@@ -1,18 +1,28 @@
+import {
+  ColorSchemeScript,
+  mantineHtmlProps,
+  MantineProvider,
+} from "@mantine/core";
 import type { PropsWithChildren } from "react";
 import { Links, Meta, Scripts, ScrollRestoration } from "react-router";
+import "@mantine/core/styles.css";
+import { theme } from "~/styles/theme";
 
 const GlobalLayout = ({ children }: PropsWithChildren) => {
   return (
-    <html lang="en">
+    <html lang="en" {...mantineHtmlProps}>
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Amrutam</title>
+        <ColorSchemeScript />
         <Meta />
         <Links />
       </head>
       <body>
-        {children}
+        <MantineProvider theme={theme} classNamesPrefix={"amrutam"}>
+          {children}
+        </MantineProvider>
         <ScrollRestoration />
         <Scripts />
       </body>
---
+++ app/styles/theme.ts
@@ -0,0 +1,29 @@
+import { createTheme } from "@mantine/core";
+
+export const theme = createTheme({
+  // colorScheme: "light",
+  // colors: {
+  //   primary: [
+  //     "#f0f4ff",
+  //     "#e1e9ff",
+  //     "#c2d3ff",
+  //     "#a3b8ff",
+  //     "#849dff",
+  //     "#6582ff",
+  //     "#4a6eff",
+  //     "#2f5aff",
+  //     "#1445ff",
+  //     "#0030e6",
+  //   ],
+  // },
+  // primaryColor: "primary",
+  // fontFamily: "Poppins, sans-serif",
+  // headings: {
+  //   fontFamily: "Poppins, sans-serif",
+  // },
+  // globalStyles: (theme) => ({
+  //   body: {
+  //     backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.colors.gray[0],
+  //   },
+  // }),
+})
