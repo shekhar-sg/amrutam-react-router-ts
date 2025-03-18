7db207aa3958cfe0ad59d1946ea4fe4562ad37cc
suraj <37866971+srgsid@users.noreply.github.com>
2025-03-18 16:03:58 +0530

    chore: uncomment Experts, Video, and Testimonials components in index

--- app/routes/_base-layout+/index.tsx
+++ app/routes/_base-layout+/index.tsx
@@ -1,8 +1,8 @@
-import DiscoverAyurveda from "app/components/templates/home/discover-ayurveda";
+// import DiscoverAyurveda from "app/components/templates/home/discover-ayurveda";
 import Stats from "app/components/templates/home/stats";
-// import Experts from "app/components/templates/home/experts";
-// import Video from "app/components/templates/home/video";
-// import Testimonials from "app/components/templates/home/testimonials";
+import Experts from "app/components/templates/home/experts";
+import Video from "app/components/templates/home/video";
+import Testimonials from "app/components/templates/home/testimonials";
 // import ConsultationProcess from "app/components/templates/home/consultation-process";
 import HeroSection from "app/components/templates/home/hero-section";
 import "node_modules/swiper/swiper-bundle.min.css";
@@ -11,12 +11,12 @@ const HomePage = () => {
   return (
     <>
       <HeroSection />
-      <DiscoverAyurveda />
+      {/*<DiscoverAyurveda />*/}
       <Stats />
-      {/*<Experts />*/}
-      {/*<Video />*/}
+      <Experts />
+      <Video />
       {/*<ConsultationProcess />*/}
-      {/*<Testimonials />*/}
+      <Testimonials />
     </>
   );
 };
