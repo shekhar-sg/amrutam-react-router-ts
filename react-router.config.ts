2b31152d4ed243c8545d9d8a9b34e12a8ff6378e
suraj <37866971+srgsid@users.noreply.github.com>
2025-03-18 15:36:10 +0530

    feat: remove future optimizations and prerender configuration from react-router

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,10 +4,4 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
-  future:{
-    unstable_optimizeDeps:true,
-  },
-  prerender:()=>{
-    return ['/']
-  }
 } satisfies Config;
