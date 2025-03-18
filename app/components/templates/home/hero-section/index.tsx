7e4d9ba8919980be672fedc29760d7845a747006
suraj <37866971+srgsid@users.noreply.github.com>
2025-03-18 14:24:09 +0530

    feat: add background SVG and enhance hero section layout

--- app/components/templates/home/hero-section/index.tsx
+++ app/components/templates/home/hero-section/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button } from "@mantine/core";
+import { Box, Button, Image, useMantineTheme } from "@mantine/core";
 import { BsShieldCheck } from "react-icons/bs";
 import { FaPhone, FaUserDoctor } from "react-icons/fa6";
 import { IoChatbubblesOutline } from "react-icons/io5";
@@ -11,58 +11,64 @@ import Typography, {
 import HeroStats from "~/components/templates/home/hero-section/hero-stats";
 import Banner from "./banner";
 
+import backgroundSvg from "./assets/curve-header.svg";
+
 const HeroSection = () => {
+  const theme = useMantineTheme();
   return (
-    <SectionWrapper
-      className={"relative flex py-14 md:py-28 md:pb-34"}
-      WrapperProps={{
-        bg: "secondary.2",
-        className: "mb-110 md:mb-60 xl:mb-40",
-      }}
-    >
-      <Box className={"w-full space-y-4 md:w-[60%]"}>
-        <Typography fontVariant={"body-small"} fw={500} c={"dark.3"}>
-          Namaste, Welcome to Amrutam{" "}
-        </Typography>
-        <Typography
-          fontVariant={"heading-xlarge"}
-          fz={{
-            ...TypographyResponsive["heading-xlarge"],
-            xl: "h3",
-          }}
-          c={"dark.9"}
-        >
-          Struggling with Lifestyle Disorders?
-          <br /> Get Expert Ayurvedic Help Today.
-        </Typography>
-        <Typography fontVariant={"heading-xsmall"} c={"dark.4"}>
-          Speak to Certified & Trusted Ayurvedic Doctors for personalized care.
-        </Typography>
-        <Button
-          component={Link}
-          to={"/"}
-          size={"xl"}
-          fullWidth
-          w={{ sm: "80%" }}
-          radius={"lg"}
-          fw={700}
-          classNames={{
-            label: "gap-x-4",
-          }}
-        >
-          <FaPhone />
-          Instant Call for FREE
-        </Button>
-        <HeroStats />
-      </Box>
-      <div></div>
-      <Banner
-        features={features}
-        className={
-          "absolute bottom-0 h-fit max-w-[calc(100%_-_32px)] translate-y-9/10 md:translate-y-3/5 lg:max-w-[calc(100%_-_48px)] xl:max-w-[calc(100%_-_200px)]"
-        }
-      />
-    </SectionWrapper>
+    <Box className={"relative"}>
+      <Image className={"absolute inset-0"} src={backgroundSvg} />
+      <SectionWrapper
+        className={"relative flex py-14 md:py-28 md:pb-34"}
+        WrapperProps={{
+          className: `mb-110 md:mb-60 xl:mb-40 bg-gradient-to-t to-[#ffffff3a] from-[#b08e3633]`,
+        }}
+      >
+        <Box className={"w-full space-y-4 md:w-[60%]"}>
+          <Typography fontVariant={"body-small"} fw={500} c={"dark.3"}>
+            Namaste, Welcome to Amrutam{" "}
+          </Typography>
+          <Typography
+            fontVariant={"heading-xlarge"}
+            fz={{
+              ...TypographyResponsive["heading-xlarge"],
+              xl: "h3",
+            }}
+            c={"dark.9"}
+          >
+            Struggling with Lifestyle Disorders?
+            <br /> Get Expert Ayurvedic Help Today.
+          </Typography>
+          <Typography fontVariant={"heading-xsmall"} c={"dark.4"}>
+            Speak to Certified & Trusted Ayurvedic Doctors for personalized
+            care.
+          </Typography>
+          <Button
+            component={Link}
+            to={"/"}
+            size={"xl"}
+            fullWidth
+            w={{ sm: "80%" }}
+            radius={"lg"}
+            fw={700}
+            classNames={{
+              label: "gap-x-4",
+            }}
+          >
+            <FaPhone />
+            Instant Call for FREE
+          </Button>
+          <HeroStats />
+        </Box>
+        <div></div>
+        <Banner
+          features={features}
+          className={
+            "absolute bottom-0 h-fit max-w-[calc(100%_-_32px)] translate-y-9/10 md:translate-y-3/5 lg:max-w-[calc(100%_-_48px)] xl:max-w-[calc(100%_-_200px)]"
+          }
+        />
+      </SectionWrapper>
+    </Box>
   );
 };
 
