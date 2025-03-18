619a35b95459e746e39367af8e6c0432d8fe7ac9
suraj <37866971+srgsid@users.noreply.github.com>
2025-03-18 16:06:46 +0530

    chore: comment out Experts and Testimonials components in index

--- app/routes/_base-layout+/index.tsx
+++ app/routes/_base-layout+/index.tsx
@@ -1,8 +1,8 @@
 // import DiscoverAyurveda from "app/components/templates/home/discover-ayurveda";
 import Stats from "app/components/templates/home/stats";
-import Experts from "app/components/templates/home/experts";
+// import Experts from "app/components/templates/home/experts";
 import Video from "app/components/templates/home/video";
-import Testimonials from "app/components/templates/home/testimonials";
+// import Testimonials from "app/components/templates/home/testimonials";
 // import ConsultationProcess from "app/components/templates/home/consultation-process";
 import HeroSection from "app/components/templates/home/hero-section";
 import "node_modules/swiper/swiper-bundle.min.css";
