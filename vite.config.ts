f9a131cf9ac79c907194753b40502d7e17d302e3
suraj <37866971+srgsid@users.noreply.github.com>
2025-03-19 01:36:55 +0530

    fix: update Vite configuration for improved plugin management and SSR support

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
 import { reactRouter } from "@react-router/dev/vite";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-import svgR from "vite-plugin-svgr";
 import tailwindcss from "@tailwindcss/vite";
-import { type Productions, productions } from "./app/utils/environment";
+import { defineConfig } from "vite";
+import svgR from "vite-plugin-svgr";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   return {
     plugins: [tailwindcss(), reactRouter(), svgR(), tsconfigPaths()],
-    esbuild: {
-      drop: productions.includes(mode.toLowerCase() as Productions)
-        ? ["console", "debugger"]
-        : undefined,
+    esbuild: {},
+    build: {
+      ssrManifest: true,
     },
     server: {
       proxy: {
