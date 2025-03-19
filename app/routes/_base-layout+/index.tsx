5d98ec0e13ac4afd5cf2f3c1881f980743a89099
suraj <37866971+srgsid@users.noreply.github.com>
2025-03-19 07:19:31 +0530

    feat: comment out unused components and styles in HomePage

--- app/routes/_base-layout+/index.tsx
+++ app/routes/_base-layout+/index.tsx
@@ -1,13 +1,13 @@
 import DiscoverAyurveda from "app/components/templates/home/discover-ayurveda";
 import Stats from "app/components/templates/home/stats";
-import Experts from "app/components/templates/home/experts";
 import Video from "app/components/templates/home/video";
-import Testimonials from "app/components/templates/home/testimonials";
 import HeroSection from "app/components/templates/home/hero-section";
-import ConsultationProcess from "app/components/templates/home/consultation-process";
-import "node_modules/swiper/swiper.min.css";
-import "node_modules/swiper/modules/pagination.min.css";
 import Partners from "~/components/templates/home/partners";
+import ConsultationProcess from "app/components/templates/home/consultation-process";
+// import Experts from "app/components/templates/home/experts";
+// import Testimonials from "app/components/templates/home/testimonials";
+// import "node_modules/swiper/swiper.min.css";
+// import "node_modules/swiper/modules/pagination.min.css";
 
 const HomePage = () => {
   return (
@@ -16,10 +16,10 @@ const HomePage = () => {
       <Partners/>
       <DiscoverAyurveda />
       <Stats />
-      <Experts />
+      {/*<Experts />*/}
       <Video />
       <ConsultationProcess />
-      <Testimonials />
+      {/*<Testimonials />*/}
     </>
   );
 };
