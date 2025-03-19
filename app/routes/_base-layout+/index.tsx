d1183e0fbf21be733baf82e94e226f8edee10ee7
suraj <37866971+srgsid@users.noreply.github.com>
2025-03-19 11:07:50 +0530

    feat: uncomment and reintroduce components and styles in index.tsx

--- app/routes/_base-layout+/index.tsx
+++ app/routes/_base-layout+/index.tsx
@@ -1,38 +1,38 @@
-// import { Skeleton } from "@mantine/core";
+import { Skeleton } from "@mantine/core";
 import ConsultationProcess from "app/components/templates/home/consultation-process";
 import DiscoverAyurveda from "app/components/templates/home/discover-ayurveda";
 import { type ExpertDoctorsSectionProps } from "app/components/templates/home/experts";
 import HeroSection from "app/components/templates/home/hero-section";
 import Stats from "app/components/templates/home/stats";
-// import Testimonials from "app/components/templates/home/testimonials";
-// import "node_modules/swiper/swiper.min.css";
-// import "node_modules/swiper/modules/pagination.min.css";
+import Testimonials from "app/components/templates/home/testimonials";
+import "node_modules/swiper/swiper.min.css";
+import "node_modules/swiper/modules/pagination.min.css";
 import Video from "app/components/templates/home/video";
-// import { lazy, Suspense } from "react";
+import { lazy, Suspense } from "react";
 import { backendClient } from "~/axios-clients/backend.server";
 import Partners from "~/components/templates/home/partners";
-// import type { Route } from "~/route-types/types/app/routes/_base-layout+/+types";
+import type { Route } from "~/route-types/types/app/routes/_base-layout+/+types";
 import type { AllDoctorsAPIResponse } from "~/store/types/api/responses/all-doctors";
 
-// const ExpertDoctorsSection = lazy(
-//   () => import("app/components/templates/home/experts"),
-// );
+const ExpertDoctorsSection = lazy(
+  () => import("app/components/templates/home/experts"),
+);
 
-const HomePage = () => {
+const HomePage = ({ loaderData }: Route.ComponentProps) => {
   return (
     <>
       <HeroSection />
       <Partners />
       <DiscoverAyurveda />
       <Stats />
-      {/*{loaderData && (*/}
-      {/*  <Suspense fallback={<Skeleton visible h={400} py={10}  />}>*/}
-      {/*    <ExpertDoctorsSection data={loaderData} />*/}
-      {/*  </Suspense>*/}
-      {/*)}*/}
+      {loaderData && (
+        <Suspense fallback={<Skeleton visible h={400} py={10}  />}>
+          <ExpertDoctorsSection data={loaderData} />
+        </Suspense>
+      )}
       <Video />
       <ConsultationProcess />
-      {/*<Testimonials />*/}
+      <Testimonials />
     </>
   );
 };
