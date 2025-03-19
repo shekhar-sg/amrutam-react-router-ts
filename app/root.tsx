7d65ae873926311878d29d0bf4a445ed519de2e0
suraj <37866971+srgsid@users.noreply.github.com>
2025-03-19 11:48:23 +0530

    feat: update root.tsx to import Mantine and Tailwind CSS styles directly

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,10 @@
 import "@fontsource-variable/dm-sans";
-import { useEffect } from "react";
+import "@mantine/core/styles.css";
+import "tailwindcss/index.css";
 import { Outlet } from "react-router";
-import StoreProvider from "~/store/Provider";
 import GlobalErrorBoundary from "~/components/pages/global-error-boundary";
 import GlobalLayout from "~/components/pages/global-layout";
-
-// import 'tailwindcss/index.css'
+import StoreProvider from "~/store/Provider";
 
 export function links() {
   return [
@@ -22,12 +21,6 @@ export const ErrorBoundary = GlobalErrorBoundary;
 export const Layout = GlobalLayout;
 
 export default function App() {
-  useEffect(() => {
-    import("tailwindcss/index.css");
-    import('@mantine/core/styles.css')
-    import("@fontsource-variable/dm-sans/wght.css");
-  }, []);
-
   return (
     <StoreProvider>
       <Outlet />
