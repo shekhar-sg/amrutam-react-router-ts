8e087cfa1135cd6b4f2f04aab5bbb728fb2ffb92
suraj <37866971+srgsid@users.noreply.github.com>
2025-03-19 06:15:47 +0530

    feat: update typescript checker configuration based on production environment

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,7 +4,7 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
-  // prerender: ["/"],
+  prerender: ["/"],
   // future: {
   //   unstable_optimizeDeps: true,
   //   // unstable_middleware: true,
