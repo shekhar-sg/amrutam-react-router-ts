5b101b50387d428a9e9e18a21c783775eb036e43
suraj <37866971+srgsid@users.noreply.github.com>
2025-03-19 07:43:56 +0530

    feat: uncomment and lazy load Experts and Testimonials components in HomePage

--- app/routes/_base-layout+/index.tsx
+++ app/routes/_base-layout+/index.tsx
@@ -2,24 +2,30 @@ import DiscoverAyurveda from "app/components/templates/home/discover-ayurveda";
 import Stats from "app/components/templates/home/stats";
 import Video from "app/components/templates/home/video";
 import HeroSection from "app/components/templates/home/hero-section";
+import { lazy } from "react";
 import Partners from "~/components/templates/home/partners";
 import ConsultationProcess from "app/components/templates/home/consultation-process";
 // import Experts from "app/components/templates/home/experts";
 // import Testimonials from "app/components/templates/home/testimonials";
-// import "node_modules/swiper/swiper.min.css";
-// import "node_modules/swiper/modules/pagination.min.css";
+import "node_modules/swiper/swiper.min.css";
+import "node_modules/swiper/modules/pagination.min.css";
+
+const Experts = lazy(() => import("app/components/templates/home/experts"));
+const Testimonials = lazy(
+  () => import("app/components/templates/home/testimonials"),
+);
 
 const HomePage = () => {
   return (
     <>
       <HeroSection />
-      <Partners/>
+      <Partners />
       <DiscoverAyurveda />
       <Stats />
-      {/*<Experts />*/}
+      <Experts />
       <Video />
       <ConsultationProcess />
-      {/*<Testimonials />*/}
+      <Testimonials />
     </>
   );
 };
