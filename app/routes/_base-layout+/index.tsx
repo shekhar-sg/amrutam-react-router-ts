fd2629606963940bec8cae749311ce98a6641834
suraj <37866971+srgsid@users.noreply.github.com>
2025-03-19 08:33:13 +0530

    feat: enable lazy loading for Experts and Testimonials components in HomePage

--- app/routes/_base-layout+/index.tsx
+++ app/routes/_base-layout+/index.tsx
@@ -2,16 +2,13 @@ import DiscoverAyurveda from "app/components/templates/home/discover-ayurveda";
 import Stats from "app/components/templates/home/stats";
 import Video from "app/components/templates/home/video";
 import HeroSection from "app/components/templates/home/hero-section";
-// import { lazy, Suspense } from "react";
 import Partners from "~/components/templates/home/partners";
 import ConsultationProcess from "app/components/templates/home/consultation-process";
-// import "node_modules/swiper/swiper.min.css";
-// import "node_modules/swiper/modules/pagination.min.css";
-
-// const Experts = lazy(() => import("app/components/templates/home/experts"));
-// const Testimonials = lazy(
-//   () => import("app/components/templates/home/testimonials"),
-// );
+import Experts from "app/components/templates/home/experts";
+import Testimonials from "app/components/templates/home/testimonials";
+import "node_modules/swiper/swiper.min.css";
+import "node_modules/swiper/modules/pagination.min.css";
+// import '@mantine/carousel/styles.css';
 
 const HomePage = () => {
   return (
@@ -20,14 +17,10 @@ const HomePage = () => {
       <Partners />
       <DiscoverAyurveda />
       <Stats />
-      {/*<Suspense>*/}
-      {/*  <Experts />*/}
-      {/*</Suspense>*/}
+      <Experts />
       <Video />
       <ConsultationProcess />
-      {/*<Suspense>*/}
-      {/*  <Testimonials />*/}
-      {/*</Suspense>*/}
+      <Testimonials />
     </>
   );
 };
