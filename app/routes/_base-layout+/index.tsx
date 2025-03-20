45221a7ae5fbd8f3e71e2d009d6989e751e4696d
suraj <37866971+srgsid@users.noreply.github.com>
2025-03-20 10:52:41 +0530

    feat: uncomment components in index.tsx and implement lazy loading for ExpertDoctorsSection

--- app/routes/_base-layout+/index.tsx
+++ app/routes/_base-layout+/index.tsx
@@ -1,22 +1,22 @@
-import { Skeleton } from "@mantine/core";
+// import { Skeleton } from "@mantine/core";
 import ConsultationProcess from "app/components/templates/home/consultation-process";
 import DiscoverAyurveda from "app/components/templates/home/discover-ayurveda";
-import { type ExpertDoctorsSectionProps } from "app/components/templates/home/experts";
+// import { type ExpertDoctorsSectionProps } from "app/components/templates/home/experts";
 import HeroSection from "app/components/templates/home/hero-section";
 import Stats from "app/components/templates/home/stats";
 import Testimonials from "app/components/templates/home/testimonials";
 import "node_modules/swiper/swiper.min.css";
 import "node_modules/swiper/modules/pagination.min.css";
 import Video from "app/components/templates/home/video";
-import { lazy, Suspense } from "react";
-import { backendClient } from "~/axios-clients/backend.server";
+// import { lazy, Suspense } from "react";
+// import { backendClient } from "~/axios-clients/backend.server";
 import Partners from "~/components/templates/home/partners";
 import type { Route } from "~/route-types/types/app/routes/_base-layout+/+types";
-import type { AllDoctorsAPIResponse } from "~/store/types/api/responses/all-doctors";
+// import type { AllDoctorsAPIResponse } from "~/store/types/api/responses/all-doctors";
 
-const ExpertDoctorsSection = lazy(
-  () => import("app/components/templates/home/experts"),
-);
+// const ExpertDoctorsSection = lazy(
+//   () => import("app/components/templates/home/experts"),
+// );
 
 const HomePage = ({ loaderData }: Route.ComponentProps) => {
   return (
@@ -25,11 +25,11 @@ const HomePage = ({ loaderData }: Route.ComponentProps) => {
       <Partners />
       <DiscoverAyurveda />
       <Stats />
-      {loaderData && (
-        <Suspense fallback={<Skeleton visible h={400} py={10} />}>
-          <ExpertDoctorsSection data={loaderData} />
-        </Suspense>
-      )}
+      {/*{loaderData && (*/}
+      {/*  <Suspense fallback={<Skeleton visible h={400} py={10} />}>*/}
+      {/*    <ExpertDoctorsSection data={loaderData} />*/}
+      {/*  </Suspense>*/}
+      {/*)}*/}
       <Video />
       <ConsultationProcess />
       <Testimonials />
@@ -39,40 +39,40 @@ const HomePage = ({ loaderData }: Route.ComponentProps) => {
 
 export default HomePage;
 
-export const loader = async () => {
-  return backendClient
-    .get<AllDoctorsAPIResponse>("/patient/doctors/all")
-    .then((res) => {
-      const { data } = res.data;
-      if (data) {
-        return data.doctors
-          .filter(
-            (d) =>
-              d.isApproved && d.photo && d.specialities.some((s) => s.active),
-          )
-          .map(
-            ({
-              _id,
-              firstname,
-              lastname,
-              experience,
-              specialities,
-              slug,
-              photo,
-            }) => {
-              return {
-                id: _id,
-                name: `${firstname} ${lastname}`.trim(),
-                experienceInYear: experience,
-                speciality: specialities[0].name,
-                slug,
-                photo,
-              } as ExpertDoctorsSectionProps["data"][number];
-            },
-          );
-      }
-    })
-    .catch(() => {
-      return undefined;
-    });
-};
+// export const loader = async () => {
+//   return backendClient
+//     .get<AllDoctorsAPIResponse>("/patient/doctors/all")
+//     .then((res) => {
+//       const { data } = res.data;
+//       if (data) {
+//         return data.doctors
+//           .filter(
+//             (d) =>
+//               d.isApproved && d.photo && d.specialities.some((s) => s.active),
+//           )
+//           .map(
+//             ({
+//               _id,
+//               firstname,
+//               lastname,
+//               experience,
+//               specialities,
+//               slug,
+//               photo,
+//             }) => {
+//               return {
+//                 id: _id,
+//                 name: `${firstname} ${lastname}`.trim(),
+//                 experienceInYear: experience,
+//                 speciality: specialities[0].name,
+//                 slug,
+//                 photo,
+//               } as ExpertDoctorsSectionProps["data"][number];
+//             },
+//           );
+//       }
+//     })
+//     .catch(() => {
+//       return undefined;
+//     });
+// };
